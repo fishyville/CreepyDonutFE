54e4d6f6ee97a93d81ddb7cd4a85518c33cb82e1
Jason Benoit Adianto <jason.adianto@binus.edu>
2025-05-24 17:45:36 +0700

    dada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from '../component/Navbar';
 import Footer from '../component/Footer';
 
 function Home() {
+  const userId = localStorage.getItem('userId');
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
